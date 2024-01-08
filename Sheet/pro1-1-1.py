2557e69e6c92d66f7c0fc8a026d5ce38b6982473
09sujal <09sujalrendke@gmail.com>
2024-01-08 12:19:57 +0530

    learn

--- Sheet/pro1-1-1.py
+++ Sheet/pro1-1-1.py
@@ -1,10 +1,3 @@
-##Write a program that takes a character as input and prints 1, 0, or -1 according to the following rules.
-##1, if the character is an uppercase alphabet (A - Z).
-##0, if the character is a lowercase alphabet (a - z).
-##-1, if the character is not an alphabet.
-## Read input as specified in the question.
-## Print output as specified in the question.
-
 a = input()
 
 if a >='A' and a<='Z':
---
+++ Sheet/pro1-1-3.py
@@ -0,0 +1,19 @@
+from typing import *
+
+def compareIfElse(a: int, b: int):
+        if a>b:
+            return 'greater'
+        elif a<b:
+            return 'smaller'
+        else:
+            return 'equal'
+        
+#with constarints
+def compareIfElse(a: int, b: int) -> str:
+    if -10**5 <= a <= 10**5 and -10**5 <= b <= 10**5:
+        if a > b:
+            return "greater"
+        elif a < b:
+            return 'smaller'
+        else:
+            return 'equal'
