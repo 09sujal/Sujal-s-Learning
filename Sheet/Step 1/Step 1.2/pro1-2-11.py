99065888b0b58599cb529f27324f530b62fb5b06
learningby RRR <134139726+09sujal@users.noreply.github.com>
2024-01-24 09:55:46 +0530

    update

--- Sheet/Step 1/Step 1.2/pro1-2-11.py
+++ Sheet/Step 1/Step 1.2/pro1-2-11.py
@@ -1,20 +1,35 @@
+#using if else statement
 # def star(n):
-#     # star = 1
-#     # for row in range(n):
-#     #     for col in range(1,star+1):
-#     #         if (i)
-#     #         print(col,end='')
-#     #     print()
-#     #     star+=1
-
-#     num = 1
+#     star = 1
 #     for i in range(n):
-#         for j in range(1, num+1):
-#             print((i + j) & 1, end=" ")
-
-#             # End the current line.
+#         for j in range(1, star + 1):
+#             if((i+j)%2==0):
+#                 print('0', end=' ')
+#             else:
+#                 print('1', end=' ')
 #         print()
+#         star +=1
 
-#         num += 1
-# z = 5   
+# z = 5
 # star(z)
+
+def star(n):
+    star = 1
+    for i in range(n):
+        for j in range(1, star + 1):
+            print((i+j)&1,end=' ')
+        print()
+        star +=1
+
+z = 5
+star(z)
+
+# for online compiler
+# def nBinaryTriangle(n: int) -> None:
+#     star = 1
+#     for i in range(n):
+#         for j in range(1,star+1):
+#             print((i+j)&1,end=' ')
+#         print()
+#         star+=1
+
