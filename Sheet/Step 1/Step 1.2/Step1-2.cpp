7551622e67c135c9e255cb6bdcbe2d9235e1559d
learningby RRR <134139726+09sujal@users.noreply.github.com>
2024-02-14 21:05:18 +0530

    update

--- Sheet/Step 1/Step 1.2/Step1-2.cpp
+++ Sheet/Step 1/Step 1.2/Step1-2.cpp
@@ -5,10 +5,120 @@ int one(int n)
 {
     for (int i = 0; i < n; i++)
     {
-        for (int col = 0; col < n; col)
+        for (int j = 0; j < n; j++)
         {
             cout << "*";
         }
-        cout<<endl;
+        cout << endl;
     }
+}
+
+int two(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+int three(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j;
+        }
+        cout << endl;
+    }
+}
+
+int four(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << i;
+        }
+        cout << endl;
+    }
+}
+
+int five(int n)
+{
+    for (int i = n; i >= 1; i--)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+int six(int n)
+{
+    for (int i = n; i >= 1; i--)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j;
+        }
+        cout << endl;
+    }
+}
+
+int seven(int n)
+{
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 0; j < 2 * i + 1; j++)
+        {
+            cout << "*";
+        }
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+
+int eight(int n)
+{
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 0; j < 2 * n - (2 * i + 1); j++)
+        {
+            cout << "*";
+        }
+        for (int j = 0; j < i; j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int a = 5;
+    eight(a);
+
+    return 0;
 }
